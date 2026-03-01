30a59fe77c474c403da84da694a360a5fbe14b4d
NicoJoya <nicolas_joya@javeriana.edu.co>
2026-03-01 01:27:20 -0500

    feat: Implementar repositorio para Marca. Closes #107

--- backend/src/main/java/com/ceiba/fashtoll/repository/BrandRepository.java
+++ backend/src/main/java/com/ceiba/fashtoll/repository/BrandRepository.java
@@ -1,4 +1,9 @@
 package com.ceiba.fashtoll.repository;
 
-public interface BrandRepository {
+import com.ceiba.fashtoll.entity.Brand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BrandRepository extends JpaRepository<Brand, Long> {
 }
