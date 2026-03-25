13cbaff998c211bf4ebabe1a3f8e85edb711cc20
NicoJoya <nicolas_joya@javeriana.edu.co>
2026-03-24 21:32:27 -0500

    fix: Dividir DTOs para la gestión de usuarios de Admin

---
+++ backend/src/main/java/com/ceiba/fashtoll/user/dto/UserUpdateRequest.java
@@ -0,0 +1,22 @@
+package com.ceiba.fashtoll.user.dto;
+
+import com.ceiba.fashtoll.enums.Role;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserUpdateRequest {
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ser un email válido")
+    private String email;
+
+    private Role role;
+    private Boolean isActive;
+}
