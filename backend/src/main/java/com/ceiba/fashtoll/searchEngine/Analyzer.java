5da6b6bacf5f62b94acf17c124e4fd438e1722e1
Alejandro Gonzalez Ochoa <alejandro.gonzalez.o1055@gmail.com>
2026-04-07 22:41:44 -0500

    feat: cambios en el Analyzer y el SearchEngine (#34) y (#214)

--- backend/src/main/java/com/ceiba/fashtoll/searchEngine/Analyzer.java
+++ backend/src/main/java/com/ceiba/fashtoll/searchEngine/Analyzer.java
@@ -4,11 +4,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Analyzer {
-    public void characterFilter(String query){
+    public void characterFilter(String rawQuery){
+        //limpia las etiquetas HTML
+        String cleanQuery = rawQuery.replaceAll("<[^>]+>", " ");
 
+        cleanQuery.replaceAll("","");
     }
 
-    public void tokenizer(){
+    public void obtainKeyWords(){
 
     }
 }
